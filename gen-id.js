d829c06c0f617141d11011fde7a435e8f3993918
mastrerahl <lordrahl254@gmail.com>
2025-07-23 08:49:05 +0300

    Update gen-id.js

--- gen-id.js
+++ gen-id.js
@@ -1,10 +1,10 @@
-// gen-id.js
-function makeid(length = 5) {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+function makeid(num = 4) {
   let result = "";
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var characters9 = characters.length;
+  for (var i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters9));
   }
   return result;
 }
-module.exports = { makeid };
+module.exports = {makeid};
